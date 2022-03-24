a9babdf49243303e785fe29870e0097d54e0fb43
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-03-23 19:04:13 -0700

    added token conversion to wei functionality and added before section

--- test/decentralBank.tests.js
+++ test/decentralBank.tests.js
@@ -6,15 +6,32 @@ require('chai')
 .use(require('chai-as-promised'))
 .should()
 
-contract('DecentralBank', (accounts) => {
+contract('DecentralBank', ([owner, customer]) => {
+    let tether, rwd, decentralBank
+
+    function tokens(number) {
+        return web3.utils.toWei(number, 'ether')
+    }
+
+    before(async () => {
+        // load contracts
+        tether = await Tether.new()
+        rwd = await RWD.new()
+        decentralBank = await DecentralBank.new(rwd.address, tether.address)
+
+        // Transfer all tokens to DecentralBank (1 million)
+        await rwd.transfer(decentralBank.address, tokens('1000000'))
+
+        // Transfer 100 mock Tethers to customer
+        await tether.transfer(customer, tokens('100'), {from: owner})
+    })
+
     // describe is telling us in the console what contract we're testing for...
     // and then launching an anonymous function
     describe('Mock Tether Deployment', () => {
         // it is telling us what we're testing within the contract...
         // and launching an async anon function
         it('matches name successfully', async () => {
-            // assigning Tether contract and all properties to 'tether'
-            let tether = await Tether.new()
             // assigning name of contract to 'name'
             const name = await tether.name()
             // assert library from chai: 'equal' compares two data types...
@@ -24,7 +41,6 @@ contract('DecentralBank', (accounts) => {
     })
     describe('RWD Deployment', () => {
         it('matches name successfully', async () => {
-            let rwd = await RWD.new()
             const name = await rwd.name()
             assert.equal(name, 'Reward Token')
         })
