ee5b3b3facbafaffd97ecadf2a2aea5c515e6986
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-03-26 23:01:51 -0700

    added beginning of web3 functions

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import '../App.css';
+import Web3 from 'web3';
 import Form from './Form';
+import Tether from
 
 function App() {
+
+  // connect wallet before component mounts
+  // async UNSAFE_componentWillMount() {
+  //   await this.loadWeb3()
+  //   await this.loadBlockchainData()
+  // }
+
   return (
     <div className="App">
         <h1 className='app-title'>Yield Staking Dapplication</h1>
